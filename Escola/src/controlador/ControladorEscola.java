0a12d92b542002f9f5531458d8f4a197fbdced9f
User <User@AMD>
2019-09-16 22:54:28 -0300

    casa

---
+++ Escola/src/controlador/ControladorEscola.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+import dao.DaoEscola;
+import javax.swing.JOptionPane;
+import modelo.Escola;
+import tela.manutencao.ManutencaoEscola;
+import java.util.List;
+
+import java.util.Vector;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author User
+ */
+public class ControladorEscola {
+
+    public static void inserir(ManutencaoEscola man){
+        Escola objeto = new Escola();
+        objeto.setNome(man.jtfNome.getText());
+        objeto.setEndereco(man.jtfEndereco.getText());
+        objeto.setSigla(man.jtfSigla.getText());
+        objeto.setNralunos(Integer.parseInt(man.jtfNralunos.getText()));
+        objeto.setArea(Double.parseDouble(man.jtfArea.getText()));
+        
+        
+        boolean resultado = DaoEscola.inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+}
+
+    public static void alterar(ManutencaoEscola man){
+        Escola objeto = new Escola();
+        //definir todos os atributos
+        objeto.setCodigo(Integer.parseInt(man.jtfCodigo.getText()));
+        objeto.setNome(man.jtfNome.getText());
+        objeto.setEndereco(man.jtfEndereco.getText());
+        objeto.setSigla(man.jtfSigla.getText());
+        objeto.setNralunos(Integer.parseInt(man.jtfNralunos.getText()));
+        objeto.setArea(Double.parseDouble(man.jtfArea.getText()));
+        
+        boolean resultado = DaoEscola.alterar(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Alterado com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+
+   public static void excluir(ManutencaoEscola man){
+        Escola objeto = new Escola();
+        objeto.setCodigo(Integer.parseInt(man.jtfCodigo.getText())); //só precisa definir a chave primeira
+        
+        boolean resultado = DaoEscola.excluir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Excluído com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+   
+    public static void atualizarTabela(JTable tabela) {
+        DefaultTableModel modelo = new DefaultTableModel();
+        //definindo o cabeçalho da tabela
+        modelo.addColumn("Código");
+        modelo.addColumn("Nome");
+        modelo.addColumn("Endereço");
+        modelo.addColumn("Sigla");
+        modelo.addColumn("Número de alunos");
+        modelo.addColumn("aÁrea");
+        List<Escola> resultados = DaoEscola.consultar();
+        for (Escola objeto : resultados) {
+            Vector linha = new Vector();
+            
+            //definindo o conteúdo da tabela
+            linha.add(objeto.getCodigo());
+            linha.add(objeto.getNome());
+            linha.add(objeto.getEndereco());
+            linha.add(objeto.getSigla());
+            linha.add(objeto.getNralunos());
+            linha.add(objeto.getArea());
+            modelo.addRow(linha); //adicionando a linha na tabela
+        }
+        tabela.setModel(modelo);
+    }
+}
---
+++ Escola/src/dao/DaoEscola.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.Escola;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author User
+ */
+public class DaoEscola {
+    public static boolean inserir(Escola objeto) {
+        String sql = "INSERT INTO escola (nome, endereco, sigla, nralunos, area) VALUES (?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setString(1, objeto.getNome());
+            ps.setString(2, objeto.getEndereco());
+            ps.setString(3, objeto.getSigla());
+            ps.setInt(4, objeto.getNralunos());
+            ps.setDouble(5, objeto.getArea());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Escola objeto = new Escola();
+        objeto.setNome("Instituto Federal");
+        objeto.setEndereco("Brasil");
+        objeto.setSigla("IFRS");
+        objeto.setNralunos(200);
+        objeto.setArea(3.2);
+        
+        
+        
+        boolean resultado = inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+    public static boolean alterar(Escola objeto) {
+        String sql = "UPDATE escola SET nome = ?, endereco = ?, sigla = ?, nralunos = ?, area = ? WHERE codigo=?";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setString(1, objeto.getNome()); 
+            ps.setString(2, objeto.getEndereco());
+            ps.setString(3, objeto.getSigla());
+            ps.setInt(4, objeto.getNralunos());
+            ps.setDouble(5, objeto.getArea());
+            ps.setInt(6, objeto.getCodigo());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+      public static boolean excluir(Escola objeto) {
+        String sql = "DELETE FROM escola WHERE codigo=?";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setInt(1, objeto.getCodigo());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+      public static List<Escola> consultar() {
+        List<Escola> resultados = new ArrayList<>();
+        //editar o SQL conforme a entidade
+        String sql = "SELECT codigo, nome, endereco, sigla, nralunos, area FROM escola";
+        PreparedStatement ps;
+        try {
+            ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Escola objeto = new Escola();
+                //definir um set para cada atributo da entidade, cuidado com o tipo
+                objeto.setCodigo(rs.getInt("codigo"));
+                objeto.setNome(rs.getString("nome"));
+                objeto.setEndereco(rs.getString("endereco"));
+                objeto.setSigla(rs.getString("sigla"));
+                objeto.setNralunos(rs.getInt("nralunos"));
+                objeto.setArea(rs.getDouble("area"));
+                
+                resultados.add(objeto);//não mexa nesse, ele adiciona o objeto na lista
+            }
+            return resultados;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+}
+}
